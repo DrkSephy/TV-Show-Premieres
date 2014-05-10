b9952916d3eeba40090839f4473ec3e96add649d
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-10 11:16:39 -0400

    Adding API call for getting TV shows and times

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,9 +1,16 @@
 app.controller("mainController", function($scope, $http){
-
-    $scope.apiKey = "[a01402d4cb067ffdbc858532cb310a3e]"
+ 
+    $scope.apiKey = "[a01402d4cb067ffdbc858532cb310a3e]";
     $scope.init = function() {
-
+        //API requires a start date
+        var today = new Date();
+        //Create the date string and ensure leading zeros if required
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+            console.log(data);
+        }).error(function(error) {
+ 
+        });
     };
-
+ 
 });
-
