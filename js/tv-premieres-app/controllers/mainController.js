bca35cb0e7421535c022e675386803814b5f9fe5
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-10 11:39:01 -0400

    Formatting API data

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,6 +1,15 @@
 app.controller("mainController", function($scope, $http){
  
     $scope.apiKey = "a01402d4cb067ffdbc858532cb310a3e";
+    /*
+     * From the Trakt.tv API Documentation, to make calls to `calendar/premieres', 
+     * the following parameters are needed: 
+     
+     * API KEY - The api key generated at trakt.tv
+     * Start - The date at which to start collecting data
+     * Days - The number of days of data to retrieve
+    
+    */
     $scope.init = function() {
         //API requires a start date
         var today = new Date();
@@ -8,6 +17,19 @@ app.controller("mainController", function($scope, $http){
         var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
         $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
             console.log(data);
+            // As the data comes in, we need to format it for our own use. 
+            // For each day, get all the episodes
+            angular.forEach(data, function(value, index){
+                // The API stores the full data separate from each episode. Save it for future use.
+                var data = value.date;
+                // For each episode, push it to our `results` array
+                angular.forEach(value.episodes, function(tvshow, index){
+                    // Create a date string from the timestamp so the user can filter using text input
+                    // Attach the full date to each episode
+                    tvshow.date = date;
+                    $scope.results.push(tvshow);
+                });
+            });
         }).error(function(error) {
  
         });
